5ef0d35cd54290ca4f2e85068b16f295de4560c4
Sayeef Rahim <sayeefrahim@gmail.com>
2025-07-08 01:27:47 +0000

    input file shown

--- flask_app.py
+++ flask_app.py
@@ -39,14 +39,11 @@ class User(db.Model):
         password_hash = generate_password_hash(password)
         length = len(password_hash)
         error_message = f"passwordhas ={self.password_hash}, len={length}"
-        flash(error_message)
-
         self.password_hash = password_hash
 
 
     def check_password(self, password):
         # error_message = f"password={password}"
-
         # print(error_message, file=sys.stderr)
         # flash(error_message)
         return check_password_hash(self.password_hash, password)
@@ -69,12 +66,12 @@ def register():
         password = request.form['password']
 
         if not username or not password:
-            flash('Username and password are required.')
+            flash('Username and password are required.','error')
             return redirect(url_for('register'))
 
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
-            flash('Username already exists. Please choose a different one.')
+            flash('Username already exists. Please choose a different one.','error')
             return redirect(url_for('register'))
 
         new_user = User(username=username)
@@ -82,7 +79,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        flash('Registration successful! Please log in.')
+        flash('Registration successful! Please log in.','success')
         return redirect(url_for('login'))
     return render_template('register.html')
 
@@ -94,22 +91,37 @@ def login():
 
         user = User.query.filter_by(username=username).first()
 
-
         if user and user.check_password(password):
-            error_message = f"user.username={user.username} password={password}"
+            error_message = f"user.username={user.username}"
             print(error_message, file=sys.stderr)
             session['username'] = user.username
-            flash('Logged in successfully!')
-            return redirect(url_for('index'))
+            flash('Logged in successfully!','success')
+            #return redirect(url_for('index'))
+            return render_template('inputdata.html')
         else:
-            flash('Invalid username or password.')
+            flash('Invalid username or password.','error')
     return render_template('login.html')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    flash('You have been logged out.')
+    flash('You have been logged out.','success')
     return redirect(url_for('index'))
+
+@app.route('/inputdata', methods=['POST'])
+def inputdata():
+    flash('inputdata called')
+    if request.method == 'POST':
+        flash('POST  called')
+        alpha = request.form['alpha']
+        beta = request.form['beta']
+        sigma_sqr = request.form['sigma_sqr']
+        mu_0 = request.form['mu_0']
+        mu_1 = request.form['mu_1']
+        test_type = request.form['test_type']
+        print(f" vals alpha '{alpha}' beta '{beta}'  sigma_sqr '{sigma_sqr}' mu_0 '{mu_0}' mu_1 '{mu_1}' ",file=sys.stderr)
+        #return calculate(float(alpha),float(beta),int(sigma_sqr),int(mu_0),int(mu_1), int(test_type))
+    return render_template('inputdata.html')
 
 # --- Run the app ---
 if __name__ == '__main__':
